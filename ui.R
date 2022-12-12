29e709b29cb62c9c79c34304d7532c7af66e4740
João Piccini <joaopic@hotmail.com>
2022-12-12 10:07:59 -0400

    Código

--- ui.R
+++ ui.R
@@ -1,11 +1,28 @@
 library(shiny)
 library(leaflet)
+library(datasets)
 
-shinyUI(
+ui <- fluidPage(
   fluidPage(title = "Mapa Lavoura Temporária",
             hr(), # inserir uma linha
             "Área Colhida em Hectares no Censo Agropecuário de 2017",
             #hr(), # inserir uma linha,
-            leafletOutput("mapapg"))
+            leafletOutput("mapapg")),
+  # Gerando uma linha com uma barra lateral
+  sidebarLayout(
 
-)
+    # Defindo a barra lateral com uma entrada
+    sidebarPanel(
+      selectInput("region", "Região:",
+                  choices=colnames(mapapg)),
+      hr(),
+      helpText("Dados do Censo Agropecuário 2017 IBGE.")
+    ),
+
+    # Criando um local para o gráfico de barras
+    mainPanel(
+      plotOutput("mapa")
+    )
+
+))
+
