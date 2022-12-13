e1ee6c5850bd63c025582a8fffbeefe0addbd378
João Augusto Piccini <105990277+JAPiccini@users.noreply.github.com>
2022-12-13 17:38:19 -0400

    Add files via upload

--- Mapa Muni Interativo.R
+++ Mapa Muni Interativo.R
@@ -10,56 +10,68 @@ library(leaflet)
 library(readr)
 library(janitor)
 
-
+#Definindo o diretório padrão
 setwd("~/Projeto")
 
+#Lendo o arquivo com os dados de lavoura temporária
 tabela01 <- read_csv("tabela01.csv")
 
-tabela01 <- tabela01 %>%
-  group_by(municipios) %>%
-  mutate(cumsum = cumsum(PG))
-
+#Definindo que o arquivo mude para dataframe
 tabela01 <- as.data.frame(tabela01)
 
+#Lendo o arquivo com a localização dos municípios
 shapefile <- st_read(here("br_municipios_2017", "BRMUE250GC_SIR.shp"))
 
+#Renomeando colunas do arquivo
 shapefile <- setNames(shapefile, c("municipios","codigo", "geometry"))
 
+#Alterando o formato da coluna do arquivo
 shapefile <- shapefile%>%
   mutate(across(c(codigo), as.numeric))
 
+#Reirando a coluna municipios do arquivo( Municípios com escrita desconfigurada)
 shapefile <- subset( shapefile, select = -municipios )
 
+#Juntado os shapes com os dados
 mapapg <- full_join(tabela01, shapefile, by = 'codigo')
 
+#Alterando NAs para 0
 mapapg[is.na(mapapg)] <- 0
 
+#Alterando a configuraçõ das colunas
 mapapg <- mapapg %>%
   mutate(across(!municipios, as.numeric))%>%
   mutate(across(c(codigo,municipios), as.numeric))
 
+#Alterando o formato do arquivo
 mapapg <- st_as_sf(mapapg)
 
+#Alterando  formato do arquivo novamente
 mapapg<- as(mapapg, "Spatial")
 
-proj4string(mapapg) 
+#Definindo a projeção o mapa
+proj4string(mapapg)
 
+#Definindo o formato da coluna municipios
 Encoding(mapapg$municipios) <- "UTF-8"
 
+#Definindo legendas e coloração do mapa
 pal <- colorBin("Greens",domain = NULL,n = 5)
 
-state_popup <- paste0("<strong>Estado: </strong>", 
-                      mapapg$municipios, 
-                      "<br><strong>Hectares: </strong>", 
+state_popup <- paste0("<strong>Estado: </strong>",
+                      mapapg$municipios,
+                      "<br><strong>Hectares: </strong>",
                       mapapg$Total)
 leaflet(data = mapapg) %>%
   addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(fillColor = ~pal(mapapg$Total), 
+  addPolygons(fillColor = ~pal(mapapg$Total),
               fillOpacity = 1.0,
-              color = "#BDBDC3", 
+              color = "#BDBDC3",
               weight = 1,
               popup = state_popup) %>%
   addLegend("bottomright", pal = pal, values = ~mapapg$Total,
             title = "Área Colhida(ha)",
             opacity = 1)
 
+#Salvando o arquivo no formato para utilizá-lo no Shiny
+saveRDS(mapapg, file = "mapapg.rds")
