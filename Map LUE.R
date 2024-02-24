6e31ca38d3047518406552fabdf74063f248549b
João Augusto Piccini <joaopic@hotmail.com>
2024-02-24 10:55:04 -0400

    Add files via upload

--- Map LUE.R
+++ Map LUE.R
@@ -2,11 +2,15 @@ library(readxl)
 library(dplyr)
 library(geobr)
 library(st)
+library(sf)
 library(leaflet)
+library(plotly)
 
 #Puxando o banco de dados
 LUE_e_TE <- read_excel("C:/Users/Desktop/Downloads/LUE e TE.xls", 
                        sheet = "Variacoes")
+
+X2017 <- read_excel("2017.xlsx")
 
 #Coletando o shapefile
 map <- read_municipality(year = 2017)
@@ -16,7 +20,26 @@ map <- map[ grep("MT", map$abbrev_state) , ]
 map <- map[ grep("Ponte Branca", map$name_muni, invert = TRUE) , ]
 
 #Juntando os dados
-data <- merge(LUE_e_TE, map, by="name_muni")
+data <- merge(LUE_e_TE, X2017, by = "name_muni")
+data <- merge(data, map, by="name_muni")
+
+#Analisando os dados
+summary(data$LUE)
+summary(data$LUE17)
+hist(data$LUE, xlab = "Eficiência", ylab = "Frequência", main = "")
+#histograma empilhado dos dois censos
+plot_ly(x = data$LUE, type = "histogram") %>%
+  layout(title = "",
+         xaxis = list(title = "Eficiência",
+                      zeroline = FALSE),
+         yaxis = list(title = "Frequência",
+                      zeroline = FALSE))
+plot_ly(x = data$LUE17, type = "histogram") %>%
+  layout(title = "",
+         xaxis = list(title = "Eficiência",
+                      zeroline = FALSE),
+         yaxis = list(title = "Frequência",
+                      zeroline = FALSE))
 
 #Alterando o formato do arquivo
 data <- st_as_sf(data)
@@ -24,13 +47,10 @@ data <- st_as_sf(data)
 #Alterando  formato do arquivo novamente
 data <- as(data, "Spatial")
 
-#Definindo a projeção o mapa
-proj4string(data)
-
 #Definindo o formato da coluna municipios
 Encoding(data$name_muni) <- "UTF-8"
 
-#Definindo legendas e coloração do mapa
+#Mapa variação LUE
 pal <- colorBin(palette = "RdYlGn", domain = data@data$LUE , na.color="transparent")
 
 state_popup <- paste0("<strong>Município: </strong>",
@@ -46,4 +66,22 @@ leaflet(data = data) %>%
               popup = state_popup) %>%
   addLegend("bottomright", pal = pal, values = ~data$LUE,
             title = "Variação Eficiência do Uso da Terra",
+            opacity = 1)
+
+#Mapa LUE 2017
+pal <- colorBin(palette = "BuGn", domain = data@data$LUE17 , na.color="transparent")
+
+state_popup <- paste0("<strong>Município: </strong>",
+                      data$name_muni,
+                      "<br><strong>Variação LUE : </strong>",
+                      data$LUE17)
+leaflet(data = data) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(fillColor = ~pal(data$LUE17),
+              fillOpacity = 1.0,
+              color = "#BDBDC3",
+              weight = 1,
+              popup = state_popup) %>%
+  addLegend("bottomright", pal = pal, values = ~data$LUE17,
+            title = "Eficiência do Uso da Terra 2017",
             opacity = 1)
--- Map TE.R
+++ Map TE.R
@@ -2,11 +2,14 @@ library(readxl)
 library(dplyr)
 library(geobr)
 library(st)
+library(sf)
 library(leaflet)
 
 #Puxando o banco de dados
 LUE_e_TE <- read_excel("C:/Users/Desktop/Downloads/LUE e TE.xls", 
                        sheet = "Variacoes")
+
+X2017 <- read_excel("2017.xlsx")
 
 #Coletando o shapefile
 map <- read_municipality(year = 2017)
@@ -16,7 +19,12 @@ map <- map[ grep("MT", map$abbrev_state) , ]
 map <- map[ grep("Ponte Branca", map$name_muni, invert = TRUE) , ]
 
 #Juntando os dados
-data <- merge(LUE_e_TE, map, by="name_muni")
+data <- merge(LUE_e_TE, X2017, by = "name_muni")
+data <- merge(data, map, by="name_muni")
+
+#Análise descritiva 
+summary(data$TE)
+summary(data$TE17)
 
 #Alterando o formato do arquivo
 data <- st_as_sf(data)
@@ -24,13 +32,10 @@ data <- st_as_sf(data)
 #Alterando  formato do arquivo novamente
 data <- as(data, "Spatial")
 
-#Definindo a projeção o mapa
-proj4string(data)
-
 #Definindo o formato da coluna municipios
 Encoding(data$name_muni) <- "UTF-8"
 
-#Definindo legendas e coloração do mapa
+#Mapa Variação TE
 pal <- colorBin(palette = "RdYlGn", domain = data@data$TE , na.color="transparent")
 
 state_popup <- paste0("<strong>Município: </strong>",
@@ -46,4 +51,22 @@ leaflet(data = data) %>%
               popup = state_popup) %>%
   addLegend("bottomright", pal = pal, values = ~data$TE,
             title = "Variação Eficiência Técnica",
+            opacity = 1)
+
+#Mapa TE 2017
+pal <- colorBin(palette = "Greens", domain = data@data$TE17 , na.color="transparent")
+
+state_popup <- paste0("<strong>Município: </strong>",
+                      data$name_muni,
+                      "<br><strong>Variação LUE : </strong>",
+                      data$TE17)
+leaflet(data = data) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(fillColor = ~pal(data$TE17),
+              fillOpacity = 1.0,
+              color = "#BDBDC3",
+              weight = 1,
+              popup = state_popup) %>%
+  addLegend("bottomright", pal = pal, values = ~data$TE17,
+            title = "Eficiência Técnica 2017",
             opacity = 1)
